7d7e0028aad729e79d87d2629630c21d26f27359
Teo Asinari <tasinari@protonmail.com>
2024-09-03 19:40:23 -0700

    More garbage text generation. Hooray

--- src/llms_diy/04_Pretraining/04.py
+++ src/llms_diy/04_Pretraining/04.py
@@ -13,7 +13,7 @@ GPT_CONFIG_124M = {
 }
 
 def text_to_token_ids(text, tokenizer):
-    encoded = tokenizer.encode(text, allowed_special=('<|endoftext|>'))
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
     encoded_tensor = torch.tensor(encoded).unsqueeze(0)
     return encoded_tensor
 
@@ -22,7 +22,18 @@ def token_ids_to_text(token_ids, tokenizer):
     return tokenizer.decode(flat.tolist())
 
 if __name__ == '__main__':
+    torch.manual_seed(123)
+    model = GPTModel(GPT_CONFIG_124M)
+    model.eval()
+
     start_context = "Every effort moves you"
     tokenizer = tiktoken.get_encoding("gpt2")
 
-    token_ids = generate_text_simple()
+    token_ids = generate_text_simple(
+        model=model,
+        idx=text_to_token_ids(start_context, tokenizer),
+        max_new_tokens=10,
+        context_size=GPT_CONFIG_124M["context_length"]
+    )
+
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
