26e1984321c5ae86025075fd2985673938ee39ee
Teo Asinari <tasinari@protonmail.com>
2024-09-08 15:07:29 -0700

    Got it to pretrain and improve prediction performance

--- src/llms_diy/04_Pretraining/04.py
+++ src/llms_diy/04_Pretraining/04.py
@@ -1,6 +1,13 @@
 import torch
 import tiktoken
-from supplementary import GPTModel, generate_text_simple, create_dataloader_v1
+from supplementary import (GPTModel,
+                           generate_text_simple,
+                           create_dataloader_v1,
+                           calc_loss_loader,
+                           calc_loss_batch,
+                           evaluate_model,
+                           generate_and_print_sample,
+                           plot_losses)
 
 
 FILE = "/home/tasinari/my_repos/Teo_LLMs_DIY/src/llms_diy" \
@@ -8,11 +15,11 @@ FILE = "/home/tasinari/my_repos/Teo_LLMs_DIY/src/llms_diy" \
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,
-    "context_length": 1024,
+    "context_length": 256,
     "emb_dim": 768,
     "n_heads": 12,
     "n_layers": 12, # num of transformer blocks
-    "drop_rate": 0.0, # disable it, not needed.
+    "drop_rate": 0.1,
     "qkv_bias": False
 }
 
@@ -25,28 +32,72 @@ def token_ids_to_text(token_ids, tokenizer):
     flat = token_ids.squeeze(0)
     return tokenizer.decode(flat.tolist())
 
-if __name__ == '__main__':
-    torch.manual_seed(123)
-    model = GPTModel(GPT_CONFIG_124M)
-    model.eval()
 
+def predict_text_experiment1(tokenizer):
     start_context = "Every effort moves you"
-    tokenizer = tiktoken.get_encoding("gpt2")
-
     token_ids = generate_text_simple(
         model=model,
         idx=text_to_token_ids(start_context, tokenizer),
         max_new_tokens=10,
         context_size=GPT_CONFIG_124M["context_length"]
     )
-
     print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+
+def get_dataloader(data):
+    return create_dataloader_v1(
+        data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=True,
+        shuffle=True,
+        num_workers=0
+    )
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs,
+                       eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+
+            generate_and_print_sample(
+                model, tokenizer, device, start_context
+            )
+
+    return train_losses, val_losses, track_tokens_seen
+
+
+if __name__ == '__main__':
+    torch.manual_seed(123)
+    model = GPTModel(GPT_CONFIG_124M)
+    model.eval()
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    # predict_text_experiment1(tokenizer)
 
     with open(FILE, "r", encoding="utf-8") as f:
         text_data = f.read()
-
-    print(text_data[:99])
-    print(text_data[-99:])
 
     total_characters = len(text_data)
     total_tokens = len(tokenizer.encode(text_data))
@@ -62,25 +113,8 @@ if __name__ == '__main__':
 
     torch.manual_seed(123)
 
-    train_loader = create_dataloader_v1(
-        train_data,
-        batch_size=2,
-        max_length=GPT_CONFIG_124M["context_length"],
-        stride=GPT_CONFIG_124M["context_length"],
-        drop_last=True,
-        shuffle=True,
-        num_workers=0
-    )
-
-    val_loader = create_dataloader_v1(
-        val_data,
-        batch_size=2,
-        max_length=GPT_CONFIG_124M["context_length"],
-        stride=GPT_CONFIG_124M["context_length"],
-        drop_last=False,
-        shuffle=False,
-        num_workers=0
-    )
+    train_loader = get_dataloader(train_data)
+    val_loader = get_dataloader(val_data)
 
     print("Train loader:\n")
     for x,y in train_loader:
@@ -89,3 +123,40 @@ if __name__ == '__main__':
     print("Validation loader:\n")
     for x,y in val_loader:
         print(x.shape, y.shape)
+
+
+    train_tokens = 0
+
+    for input_batch, target_batch in train_loader:
+        train_tokens += input_batch.numel()
+
+    val_tokens = 0
+    for input_batch, target_batch in val_loader:
+        val_tokens += input_batch.numel()
+
+    print("Training tokens:", train_tokens)
+    print("Validation tokens:", val_tokens)
+    print("All tokens:", train_tokens + val_tokens)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    torch.manual_seed(123)
+
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device)
+        val_loss = calc_loss_loader(val_loader, model, device)
+
+    print("Training loss:", train_loss)
+    print("Validation loss:", val_loss)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0004, weight_decay=0.1)
+
+    num_epochs = 10
+    train_losses, val_losses, tokens_seen = train_model_simple(
+        model, train_loader, val_loader, optimizer, device, num_epochs=num_epochs,
+        eval_freq=5, eval_iter=5, start_context="Every effort moves you", tokenizer=tokenizer
+    )
+
+    torch.save(model.state_dict(), "model.pth")
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
