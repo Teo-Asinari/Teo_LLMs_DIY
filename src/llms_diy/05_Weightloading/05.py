f5d3c8ada07c2826e87c8bd34a176389a1d41962
Teo Asinari <tasinari@protonmail.com>
2024-10-26 10:54:02 -0700

    Loaded weights from GPT2 into the model. The output is a little weird. Lots of space chars after

--- src/llms_diy/05_Weightloading/05.py
+++ src/llms_diy/05_Weightloading/05.py
@@ -3,8 +3,17 @@ from importlib.metadata import version
 import torch.nn
 import torch
 import numpy as np
+import tiktoken
 
 from gpt_download import download_and_load_gpt2
+
+from supplementary import (
+    generate_text_simple,
+    text_to_token_ids,
+    token_ids_to_text
+)
+
+from supplementary import GPTModel
 
 def assign(left, right):
     if left.shape != right.shape:
@@ -19,125 +28,142 @@ def load_weights_into_gpt(gpt, params):
     for b in range(len(params["blocks"])):
         q_w, k_w, v_w = np.split((params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
 
-        gpt.trf_blocks[b].att.W_query.weight = assign(
-            gpt.trf_blocks[b].att.W_query.weight, q_w.T
-        )
-        gpt.trf_blocks[b].att.W_key.weight = assign(
-            gpt.trf_blocks[b].att.W_key.weight, k_w.T
-        )
-        gpt.trf_blocks[b].att.W_value.weight = assign(
-            gpt.trf_blocks[b].att.W_value.weight, v_w.T
-        )
+        set_att_W_weight(b, gpt, k_w, q_w, v_w)
+        set_att_W_bias(b, gpt, params)
+        set_att_out_proj(b, gpt, params)
+
+        set_layers(b, gpt, params)
+        set_norms(b, gpt, params)
+
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
 
 
-        q_b, k_b, v_b = np.split(
-            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
-        gpt.trf_blocks[b].att.W_query.bias = assign(
-            gpt.trf_blocks[b].att.W_query.bias, q_b
-        )
-        gpt.trf_blocks[b].att.W_key.bias = assign(
-            gpt.trf_blocks[b].att.W_key.bias, k_b
-        )
-        gpt.trf_blocks[b].att.W_value.bias = assign(
-            gpt.trf_blocks[b].att.W_value.bias, v_b
-        )
+def set_layers(b, gpt, params):
+    set_layer0(b, gpt, params)
+    set_layer2(b, gpt, params)
 
-        gpt.trf_blocks[b].att.out_proj.weight = assign(
-            gpt.trf_blocks[b].att.out_proj.weight,
-            params["blocks"][b]["attn"]["c_proj"]["w"].T
-        )
-        gpt.trf_blocks[b].att.out_proj.bias = assign(
-            gpt.trf_blocks[b].att.out_proj.bias,
-            params["blocks"][b]["attn"]["c_proj"]["b"]
-        )
 
-        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+def set_norms(b, gpt, params):
+    set_norm1(b, gpt, params)
+    set_norm2(b, gpt, params)
+
+
+def set_norm2(b, gpt, params):
+    gpt.trf_blocks[b].norm2.scale = assign(
+        gpt.trf_blocks[b].norm2.scale,
+        params["blocks"][b]["ln_2"]["g"])
+    gpt.trf_blocks[b].norm2.shift = assign(
+        gpt.trf_blocks[b].norm2.shift,
+        params["blocks"][b]["ln_2"]["b"])
+
+
+def set_norm1(b, gpt, params):
+    gpt.trf_blocks[b].norm1.scale = assign(
+        gpt.trf_blocks[b].norm1.scale,
+        params["blocks"][b]["ln_1"]["g"])
+    gpt.trf_blocks[b].norm1.shift = assign(
+        gpt.trf_blocks[b].norm1.shift,
+        params["blocks"][b]["ln_1"]["b"])
+
+
+def set_layer2(b, gpt, params):
+    gpt.trf_blocks[b].ff.layers[2].weight = assign(
+        gpt.trf_blocks[b].ff.layers[2].weight,
+        params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+    gpt.trf_blocks[b].ff.layers[2].bias = assign(
+        gpt.trf_blocks[b].ff.layers[2].bias,
+        params["blocks"][b]["mlp"]["c_proj"]["b"])
+
+
+def set_layer0(b, gpt, params):
+    gpt.trf_blocks[b].ff.layers[0].weight = assign(
             gpt.trf_blocks[b].ff.layers[0].weight,
-            params["blocks"][b]["mlp"]["c_fc"]["w"].T
-        )
-        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+    gpt.trf_blocks[b].ff.layers[0].bias = assign(
             gpt.trf_blocks[b].ff.layers[0].bias,
-            params["blocks"][b]["mlp"]["c_fc"]["w"].T
-        )
+            params["blocks"][b]["mlp"]["c_fc"]["b"])
 
-        gpt.trf_blocks[b].ff.layers[2].weight = assign(
-            gpt.trf_blocks[b].ff.layers[2].weight,
-            params["blocks"][b]["mlp"]["c_proj"]["w"].T
-        )
-        gpt.trf_blocks[b].ff.layers[2].bias = assign(
-            gpt.trf_blocks[b].ff.layers[2].bias,
-            params["blocks"][b]["mlp"]["c_proj"]["b"]
-        )
-
-        gpt.trf_blocks[b].norm1.scale = assign(
-            gpt.trf_blocks[b].norm1.scale,
-            params["blocks"][b]["ln_1"]["g"]
-        )
-        gpt.trf_blocks[b].norm1.shift = assign(
-            gpt.trf_blocks[b].norm1.shift,
-            params["blocks"][b]["ln_1"]["b"]
-        )
-
-        gpt.trf_blocks[b].norm2.scale = assign(
-            gpt.trf_blocks[b].norm2.scale,
-            params["blocks"][b]["ln_2"]["g"]
-        )
-        gpt.trf_blocks[b].norm2.shift = assign(
-            gpt.trf_blocks[b].norm2.shift,
-            params["blocks"][b]["ln_2"]["b"]
-        )
+def set_att_out_proj(b, gpt, params):
+    gpt.trf_blocks[b].att.out_proj.weight = assign(
+        gpt.trf_blocks[b].att.out_proj.weight,
+        params["blocks"][b]["attn"]["c_proj"]["w"].T)
+    gpt.trf_blocks[b].att.out_proj.bias = assign(
+        gpt.trf_blocks[b].att.out_proj.bias,
+        params["blocks"][b]["attn"]["c_proj"]["b"])
 
 
+def set_att_W_bias(b, gpt, params):
+    q_b, k_b, v_b = np.split(
+        (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+    gpt.trf_blocks[b].att.W_query.bias = assign(
+        gpt.trf_blocks[b].att.W_query.bias, q_b)
+    gpt.trf_blocks[b].att.W_key.bias = assign(
+        gpt.trf_blocks[b].att.W_key.bias, k_b)
+    gpt.trf_blocks[b].att.W_value.bias = assign(
+        gpt.trf_blocks[b].att.W_value.bias, v_b)
 
+
+def set_att_W_weight(b, gpt, k_w, q_w, v_w):
+    gpt.trf_blocks[b].att.W_query.weight = assign(
+        gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+    gpt.trf_blocks[b].att.W_key.weight = assign(
+        gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+    gpt.trf_blocks[b].att.W_value.weight = assign(
+        gpt.trf_blocks[b].att.W_value.weight, v_w.T)
 
 if __name__ == "__main__":
-    pkgs = [
-        "matplotlib",
-        "numpy",
-        "tiktoken",
-        "torch"
-    ]
+    settings, params = download_and_load_gpt2(model_size="124M", models_dir="gpt2")
 
-for p in pkgs:
-    print(f"{p} version: {version(p)}")
+# print("Settings: ", settings)
+# print("Parameter dictionary keys: ", params.keys())
+# # print("Params: ", params)
+#
+# #print dict keys of first transformer block
+# print(params['blocks'][0].keys())
+# print(params['blocks'][0]["mlp"].keys())
+# print(params['blocks'][0]["mlp"]['c_fc'].keys())
+# print(params['blocks'][0]["mlp"]['c_fc']['w'].shape)
+#
 
-settings, params = download_and_load_gpt2(model_size="124M", models_dir="gpt2")
+    GPT_CONFIG_124M = {
+        "vocab_size": 50257,
+        "context_length": 256,
+        "emb_dim": 768,
+        "n_heads": 12,
+        "n_layers": 12,
+        "drop_rate": 0.1,
+        "qkv_bias": False
+    }
 
-print("Settings: ", settings)
-print("Parameter dictionary keys: ", params.keys())
-# print("Params: ", params)
+    model_configs = {
+        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+    }
 
-#print dict keys of first transformer block
-print(params['blocks'][0].keys())
-print(params['blocks'][0]["mlp"].keys())
-print(params['blocks'][0]["mlp"]['c_fc']['w'].shape())
+    model_name = "gpt2-small (124M)"
+    NEW_CONFIG = GPT_CONFIG_124M.copy()
+    NEW_CONFIG.update(model_configs[model_name])
+    NEW_CONFIG.update({"context_length": 1024, "qkv_bias": True})
 
+    gpt = GPTModel(NEW_CONFIG)
+    gpt.eval()
 
-GPT_CONFIG_124M = {
-    "vocab_size": 50257,
-    "context_length": 256,
-    "emb_dim": 768,
-    "n_heads": 12,
-    "n_layers": 12,
-    "drop_rate": 0.1,
-    "qkv_bias": False
-}
+    tokenizer = tiktoken.get_encoding("gpt2")
 
-model_configs = {
-    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
-    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
-    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
-    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
-}
+    torch.manual_seed(123)
 
-model_name = "gpt2-small (124M)"
-NEW_CONFIG = GPT_CONFIG_124M.copy()
-NEW_CONFIG.update(model_configs[model_name])
-NEW_CONFIG.update({"context_length": 1024, "qkv_bias": True})
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    load_weights_into_gpt(gpt, params)
 
-from supplementary import GPTModel
+    token_ids = generate_text_simple(
+        model=gpt,
+        idx=text_to_token_ids("Every effort moves you", tokenizer).to(device),
+        max_new_tokens=50,
+        context_size=GPT_CONFIG_124M["context_length"]
+    )
 
-gpt = GPTModel(NEW_CONFIG)
-gpt.eval()
-
-
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
